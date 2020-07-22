36767d4ed249f4abe03b4f52adbe8b65a7814587
Johanes Nathanael <johanes@Johaness-MacBook-Pro.local>
2020-07-23 01:40:06 +0800

    update code

--- CTCI 8_11.py
+++ CTCI 8_11.py
@@ -2,17 +2,24 @@
 	Coins : Given an infinite number of quarters (25 cents), dimes (10 cents), nickels (5 cents), and pennies (1 cent), write code to calculate the number of ways of representing n cents.
 '''
 
-def coins (n,):
-	if n == 0:
+# geeksforgeeks method 
+
+def coins (arr, m, n):
+	if (n == 0):
 		return 1
 
-	if n < 0:
-		return 0:
+	if (n < 0): 
+		return 0
 
+	if (m <= 0 and n >= 1):
+		return 0
+
+	return coins(arr, m-1, n) + coins(arr, m, n-arr[m-1])
 
 if __name__ == '__main__':
-	n = [25, 10, 5, 1]
-
-	result = coins (n)
+	arr = [25, 10, 5, 1]
+	n = 25
+	m = len(arr)
+	result = coins (arr, m, n)
 
 	print ('result: ', result)
