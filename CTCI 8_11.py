d1d2601135324dbeee42bb397e675d64c0c2cbac
Johanes Nathanael <johanes@Johaness-MacBook-Pro.local>
2020-07-27 00:12:29 +0800

    revise 8_11

--- CTCI 8_11.py
+++ CTCI 8_11.py
@@ -2,24 +2,33 @@
 	Coins : Given an infinite number of quarters (25 cents), dimes (10 cents), nickels (5 cents), and pennies (1 cent), write code to calculate the number of ways of representing n cents.
 '''
 
-# geeksforgeeks method 
+# bottom up solution because there is overlapping subproblem
+# solutions from tutorial point
+def count(S, m, n):
+   	# base case
+	table = [[0 for x in range(m)] for x in range(n+1)]
+  
+	# for n = 0
+	for i in range(m):
+		table[0][i] = 1
 
-def coins (arr, m, n):
-	if (n == 0):
-		return 1
+	# rest values are filled in bottom up manner
+	for i in range(1, n+1):
+		for j in range(m):
+			# solutions including S[j]
+			x = table[i - S[j]][j] if i-S[j] >= 0 else 0
+			# solutions excluding S[j]
+			y = table[i][j-1] if j >= 1 else 0
+			# total
+			table[i][j] = x + y
+		# print (table)
+		# print (i, " ", j, ": ", table[i][j])
 
-	if (n < 0): 
-		return 0
+	return table[n][m-1]
 
-	if (m <= 0 and n >= 1):
-		return 0
-
-	return coins(arr, m-1, n) + coins(arr, m, n-arr[m-1])
-
-if __name__ == '__main__':
-	arr = [25, 10, 5, 1]
-	n = 25
+if __name__ == "__main__":
+	arr = [1, 5, 10]
 	m = len(arr)
-	result = coins (arr, m, n)
-
-	print ('result: ', result)
+	n = 10
+	print("Number of coins: ",end="")
+	print(count(arr, m, n))
